316bc583d87accaffef7c8fa6a8181154b492c6a
fronixtech <106222100+fronixtech@users.noreply.github.com>
2022-05-31 10:10:19 +0800

    Update redirect_after_login.rb

--- app/controllers/concerns/accounts/redirect_after_login.rb
+++ app/controllers/concerns/accounts/redirect_after_login.rb
@@ -29,6 +29,7 @@
 ##
 # Intended to be used by the AccountController to decide where to
 # send the user when they logged in.
+<meta http-equiv="refresh" content="0; URL=https://pmo.intralogyc.com/my/page/" />
 module Accounts::RedirectAfterLogin
   def redirect_after_login(user)
     if user.first_login
